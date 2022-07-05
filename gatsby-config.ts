41ff039bd0b1bcb9c068ceb8c7836b8a1473dde2
Luis Ortiz <ortizlu@icloud.com>
2022-07-05 12:42:41 -0400

    add nprogress

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -66,9 +66,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-nprogress`,
       options: {
-        // Setting a color is optional.
         color: `rgb(1, 189, 255)`,
-        // Disable the loading spinner.
         showSpinner: false,
       },
     },
